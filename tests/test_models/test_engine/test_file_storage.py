3c88200b03924fbe7a76a298a09283c333a47bf1
Almujtaba Abdalla <almujtaba007@gmail.com>
2024-08-05 15:31:24 -0700

    fix stupid typo mistake

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -39,7 +39,7 @@ class TestFileStorageDocs(unittest.TestCase):
 
     def test_pep8_conformance_test_file_storage(self):
         """Test tests/test_models/test_file_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quite=True)
+        pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(
                 ['tests/test_models/test_engine/test_file_storage.py']
                 )
